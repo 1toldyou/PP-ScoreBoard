9fd43e610e738b506b50478e8be62edae327a64e
1toldyou <86680163+1toldyou@users.noreply.github.com>
2022-12-03 17:35:29 -0800

    calculate score?

--- src/lib/scoring.ts
+++ src/lib/scoring.ts
@@ -4,24 +4,46 @@ import {Alliance, ConeType} from "./cone";
 export interface Scoring {
     blueTotal: number;
     redTotal: number;
+    blueAutoCone?: number;
+    redAutoCone?: number;
+    blueTeleopCone?: number;
+    redTeleopCone?: number;
 }
 
 export function computeScore(actions: GameAction[]): Scoring{
+    let currentConeScoreMultiplier: number;
     let blueTotal = 0;
     let redTotal = 0;
+    let blueAutoCone = 0;
+    let redAutoCone = 0;
 
     actions.forEach(action => {
-        const currentConeScoreMultiplier = action.cone.category === ConeType.AUTO ? 2 : 1;
-        if (action.cone.color == Alliance.BLUE) {
-            blueTotal += action.pole.height * currentConeScoreMultiplier;
-        }
-        else if (action.cone.color == Alliance.RED) {
-            redTotal += action.pole.height * currentConeScoreMultiplier;
+        if (action.cone.category === ConeType.AUTO) {
+            currentConeScoreMultiplier = 2;
+            if (action.cone.color == Alliance.BLUE) {
+                blueAutoCone += action.pole.height * currentConeScoreMultiplier;
+            }
+            else if (action.cone.color == Alliance.RED) {
+                redAutoCone += action.pole.height * currentConeScoreMultiplier;
+            }
+        } else if (action.cone.category === ConeType.REGULAR) {
+            currentConeScoreMultiplier = 1;
+            if (action.cone.color == Alliance.BLUE) {
+                blueTotal += action.pole.height * currentConeScoreMultiplier;
+            }
+            else if (action.cone.color == Alliance.RED) {
+                redTotal += action.pole.height * currentConeScoreMultiplier;
+            }
         }
     });
+
+    blueTotal += blueAutoCone;
+    redTotal += redAutoCone;
 
     return {
         blueTotal: blueTotal,
         redTotal: redTotal,
+        blueAutoCone: blueAutoCone,
+        redAutoCone: redAutoCone,
     }
 }
